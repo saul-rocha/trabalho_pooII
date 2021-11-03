fed90132c136842c1cc67000a707c05e2b7c3f60
Saul Rocha <55061070+saul-rocha@users.noreply.github.com>
2021-11-03 16:56:37 -0300

    Criação do bd e conexão cliente-servidor
    Login, deposito e extrato :white_check_mark:

--- main_multitela.py
+++ main_multitela.py
@@ -1,5 +1,5 @@
 import sys
-
+import socket
 
 from PyQt5 import QtCore, QtGui, QtWidgets
 from PyQt5.QtWidgets import QMainWindow, QApplication, QFileDialog, QMessageBox
@@ -21,110 +21,122 @@ from src.cliente import Client
 from src.autentica import Autenticavel
 from src.auth import SistemaInterno
 
-class Ui_Main(QtWidgets.QWidget):
-    def setupUi(self, Main):
-        Main.setObjectName('Main')
-        Main.resize(640, 480)
+ip = "localhost"
+port = 8000
+addr = ((ip,port)) ##tupla  de endereço
 
-        self.QtStack = QtWidgets.QStackedLayout()
+cliente_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM) #AF_INET parametro para informar a familia do protocolo, SOCK_STREAM indica que é TCP/IP
+cliente_socket.connect(addr) #conexão
+mensagem = ''
+while(mensagem != '/quit'):
+    class Ui_Main(QtWidgets.QWidget):
+        def setupUi(self, Main):
+            Main.setObjectName('Main')
+            Main.resize(640, 480)
 
-        self.stack0 = QtWidgets.QMainWindow() 
-        self.stack1 = QtWidgets.QMainWindow() 
-        self.stack2 = QtWidgets.QMainWindow() 
-        self.stack3 = QtWidgets.QMainWindow()
-        self.stack4 = QtWidgets.QMainWindow()
-        self.stack5 = QtWidgets.QMainWindow()
-        self.stack6 = QtWidgets.QMainWindow()
-        self.stack7 = QtWidgets.QMainWindow()
+            self.QtStack = QtWidgets.QStackedLayout()
 
-        self.tela_main_menu = Principal()
-        self.tela_main_menu.setupUi(self.stack0)
+            self.stack0 = QtWidgets.QMainWindow() 
+            self.stack1 = QtWidgets.QMainWindow() 
+            self.stack2 = QtWidgets.QMainWindow() 
+            self.stack3 = QtWidgets.QMainWindow()
+            self.stack4 = QtWidgets.QMainWindow()
+            self.stack5 = QtWidgets.QMainWindow()
+            self.stack6 = QtWidgets.QMainWindow()
+            self.stack7 = QtWidgets.QMainWindow()
 
-        self.tela_login = Login()
-        self.tela_login.setupUi(self.stack1)
+            self.tela_main_menu = Principal()
+            self.tela_main_menu.setupUi(self.stack0)
 
-        self.tela_cadastrar = Cadastrar()
-        self.tela_cadastrar.setupUi(self.stack2)
+            self.tela_login = Login()
+            self.tela_login.setupUi(self.stack1)
 
-        self.tela_deposita = Deposita()
-        self.tela_deposita.setupUi(self.stack3)
+            self.tela_cadastrar = Cadastrar()
+            self.tela_cadastrar.setupUi(self.stack2)
 
-        self.tela_extrato = Extrato()
-        self.tela_extrato.setupUi(self.stack4)
+            self.tela_deposita = Deposita()
+            self.tela_deposita.setupUi(self.stack3)
 
-        self.tela_saque = Saque()
-        self.tela_saque.setupUi(self.stack5)
+            self.tela_extrato = Extrato()
+            self.tela_extrato.setupUi(self.stack4)
 
-        self.tela_transferencia = Transferencia()
-        self.tela_transferencia.setupUi(self.stack6)
+            self.tela_saque = Saque()
+            self.tela_saque.setupUi(self.stack5)
 
-        self.tela_home = Home()
-        self.tela_home.setupUi(self.stack7)
+            self.tela_transferencia = Transferencia()
+            self.tela_transferencia.setupUi(self.stack6)
 
-        self.QtStack.addWidget(self.stack0)
-        self.QtStack.addWidget(self.stack1)
-        self.QtStack.addWidget(self.stack2)
-        self.QtStack.addWidget(self.stack3)
-        self.QtStack.addWidget(self.stack4)
-        self.QtStack.addWidget(self.stack5)
-        self.QtStack.addWidget(self.stack6)
-        self.QtStack.addWidget(self.stack7)
+            self.tela_home = Home()
+            self.tela_home.setupUi(self.stack7)
 
-
-class Main(QMainWindow, Ui_Main):
-    def __init__(self, parent=None):
-        super(Main, self).__init__(parent)
-        self.setupUi(self)
-
-        self.cad = Cadastro()
-        self.si = SistemaInterno()
-
-        self.tela_main_menu.pushButton.clicked.connect(self.abrirTelaLogin)
-        self.tela_main_menu.pushButton_2.clicked.connect(self.abrirTelaCadastro)
-
-        self.tela_login.pushButton.clicked.connect(self.botaoEntrar)
-        self.tela_login.pushButton_2.clicked.connect(self.botaoVoltar)
-
-        self.tela_cadastrar.pushButton.clicked.connect(self.botaoOk)
-        self.tela_cadastrar.pushButton_2.clicked.connect(self.botaoVoltar)
-
-        self.tela_home.pushButton_2.clicked.connect(self.abrirTelaDeposita)
-        self.tela_deposita.pushButton.clicked.connect(self.botaoDeposita)
-        self.tela_deposita.pushButton_2.clicked.connect(self.abrirTelaHome)
+            self.QtStack.addWidget(self.stack0)
+            self.QtStack.addWidget(self.stack1)
+            self.QtStack.addWidget(self.stack2)
+            self.QtStack.addWidget(self.stack3)
+            self.QtStack.addWidget(self.stack4)
+            self.QtStack.addWidget(self.stack5)
+            self.QtStack.addWidget(self.stack6)
+            self.QtStack.addWidget(self.stack7)
 
 
-        self.tela_home.pushButton.clicked.connect(self.abrirTelaSacar)
-        self.tela_saque.pushButton.clicked.connect(self.botaoSacar)
-        self.tela_saque.pushButton_2.clicked.connect(self.abrirTelaHome)
-        
+    class Main(QMainWindow, Ui_Main):
+        loginCpf = ''
+        def __init__(self, parent=None):
+            super(Main, self).__init__(parent)
+            self.setupUi(self)
 
-        self.tela_home.pushButton_4.clicked.connect(self.abrirTelaTransferencia)
-        self.tela_transferencia.pushButton.clicked.connect(self.botaoTransfere)
-        self.tela_transferencia.pushButton_2.clicked.connect(self.abrirTelaHome)
+            self.cad = Cadastro()
+            self.si = SistemaInterno()
 
-        
-        self.tela_home.pushButton_3.clicked.connect(self.botaoExtrato)
+            self.tela_main_menu.pushButton.clicked.connect(self.abrirTelaLogin)
+            self.tela_main_menu.pushButton_2.clicked.connect(self.abrirTelaCadastro)
 
-        self.tela_extrato.pushButton_2.clicked.connect(self.abrirTelaHome)
+            self.tela_login.pushButton.clicked.connect(self.botaoEntrar)
+            self.tela_login.pushButton_2.clicked.connect(self.botaoVoltar)
 
-        self.tela_home.pushButton_5.clicked.connect(self.botaoVoltar)
+            self.tela_cadastrar.pushButton.clicked.connect(self.botaoOk)
+            self.tela_cadastrar.pushButton_2.clicked.connect(self.botaoVoltar)
 
-        #self.tela_extrato.pushButton.clicked.connect(self.botaoExtrato)
-        #self.tela_extrato.pushButton_2.clicked.connect(self.botaoVoltar)
+            self.tela_home.pushButton_2.clicked.connect(self.abrirTelaDeposita)
+            self.tela_deposita.pushButton.clicked.connect(self.botaoDeposita)
+            self.tela_deposita.pushButton_2.clicked.connect(self.abrirTelaHome)
 
 
-    def botaoOk(self):
-            nome = self.tela_cadastrar.lineEdit.text()
-            sobrenome = self.tela_cadastrar.lineEdit_4.text()
-            cpf = self.tela_cadastrar.lineEdit_6.text()
-            numero = self.tela_cadastrar.lineEdit_7.text()
-            limite = self.tela_cadastrar.lineEdit_8.text()
-            senha = self.tela_cadastrar.lineEdit_9.text()
+            self.tela_home.pushButton.clicked.connect(self.abrirTelaSacar)
+            self.tela_saque.pushButton.clicked.connect(self.botaoSacar)
+            self.tela_saque.pushButton_2.clicked.connect(self.abrirTelaHome)
+            
 
-            if not(nome == '' or sobrenome == '' or cpf == '' or numero == '' or limite == '' or senha == ''):
-                cliente = Client(nome,sobrenome,cpf)
-                c = Conta(numero, cliente, 0.0, limite, senha)
-                if(self.cad.cadastra(c)):
+            self.tela_home.pushButton_4.clicked.connect(self.abrirTelaTransferencia)
+            self.tela_transferencia.pushButton.clicked.connect(self.botaoTransfere)
+            self.tela_transferencia.pushButton_2.clicked.connect(self.abrirTelaHome)
+
+            
+            self.tela_home.pushButton_3.clicked.connect(self.botaoExtrato)
+
+            self.tela_extrato.pushButton_2.clicked.connect(self.abrirTelaHome)
+
+            self.tela_home.pushButton_5.clicked.connect(self.botaoVoltar)
+
+            #self.tela_extrato.pushButton.clicked.connect(self.botaoExtrato)
+            #self.tela_extrato.pushButton_2.clicked.connect(self.botaoVoltar)
+
+
+        def botaoOk(self):
+                nome = self.tela_cadastrar.lineEdit.text()
+                sobrenome = self.tela_cadastrar.lineEdit_4.text()
+                cpf = self.tela_cadastrar.lineEdit_6.text()
+                numero = self.tela_cadastrar.lineEdit_7.text()
+                limite = self.tela_cadastrar.lineEdit_8.text()
+                senha = self.tela_cadastrar.lineEdit_9.text()
+                '''global identifica que a variavel mensagem é global'''
+                global mensagem
+                mensagem = 'cad,'+ nome+','+sobrenome+','+ cpf+','+numero+','+limite+','+senha
+                cliente_socket.send(mensagem.encode())
+                control = cliente_socket.recv(1024).decode()
+                mensagem = ''
+
+                if (control == 'true'):
                     QMessageBox.information(None, 'GP Bank', 'Cadastro realizado com sucesso!')
                     self.tela_cadastrar.lineEdit.setText('')
                     self.tela_cadastrar.lineEdit_4.setText('')
@@ -132,121 +144,133 @@ class Main(QMainWindow, Ui_Main):
                     self.tela_cadastrar.lineEdit_7.setText('')
                     self.tela_cadastrar.lineEdit_8.setText('')
                     self.tela_cadastrar.lineEdit_9.setText('')
+                elif(control =='vazio'):
+                    QMessageBox.information(None, 'GP Bank', 'Todas as informações devem ser preenchidas!')
                 else:
                     QMessageBox.information(None, 'GP Bank', 'CPF informado já existe!')
-            else:
-                QMessageBox.information(None, 'GP Bank', 'Todas as informações devem ser preenchidas!')
 
 
-    def botaoEntrar(self):
-        cpf = self.tela_login.lineEdit.text()
-        senha = self.tela_login.lineEdit_2.text()
-
-        conta = self.cad.busca_cpf(cpf)
-        if(conta != None):
-            if(self.si.login(conta, cpf, senha) == True):
+        def botaoEntrar(self):
+            cpf = self.tela_login.lineEdit.text()
+            senha = self.tela_login.lineEdit_2.text()
+            global mensagem
+            mensagem = ''
+            mensagem = 'login,'+cpf+','+senha
+            cliente_socket.send(mensagem.encode())
+            control = cliente_socket.recv(1024).decode()
+            if(control != 'none' and control != 'false'):
                 self.QtStack.setCurrentIndex(7)
-                self.tela_home.lineEdit.setText(str(conta.saldo))
-                self.tela_home.lineEdit_2.setText(str(conta.limite))
+            elif(control == 'false'):
+                QMessageBox.information(None, 'GP Bank', 'Todos os campos devem ser preeenchidos!')
 
-            else:
-                
+            else:   
                 QMessageBox.information(None, 'GP Bank', 'Login incorreto!')
-                
-        else:
-            QMessageBox.information(None, 'GP Bank', 'Nenhuma conta cadastrada neste CPF!') 
+                    
 
-        #self.tela_login.lineEdit.setText('')
-        #self.tela_login.lineEdit_2.setText('')
+            #self.tela_login.lineEdit.setText('')
+            #self.tela_login.lineEdit_2.setText('')
 
-    def botaoDeposita(self):
-        valor = 0.0
-        valor = self.tela_deposita.lineEdit_2.text()
+        def botaoDeposita(self):
+            global mensagem
+            
+            valor = self.tela_deposita.lineEdit_2.text()
+            cpf = self.tela_login.lineEdit.text()
+            
 
-        cpf = self.tela_login.lineEdit.text()
-        conta = self.cad.busca_cpf(cpf)
-        res = conta.deposita(valor)
+            mensagem = 'deposito,'+valor+','+cpf
+            cliente_socket.send(mensagem.encode())
+            control = cliente_socket.recv(1024).decode()
+            mensagem = ''
+            res = control.split(',')
+            if(res[0] == 'false'):
+                QMessageBox.information(None, 'GP Bank', 'Valor inválido!')
+            elif(res[0] == 'false1'):
+                QMessageBox.information(None, 'GP Bank', 'CPF inválido!')
+            elif(res[0] == 'false2'):
+                QMessageBox.information(None, 'GP Bank', 'Todos os campos devem ser preeenchidos!')
+            else:
+                QMessageBox.information(None, 'GP Bank', 'Depósito efetuado!')   
+                self.tela_home.lineEdit.setText(res[1])
 
-        if res == False:
-            QMessageBox.information(None, 'GP Bank', 'Valor inválido!')
-        else:
-            QMessageBox.information(None, 'GP Bank', 'Depósito efetuado!')   
 
-        self.tela_home.lineEdit.setText(str(conta.saldo))
-        self.tela_home.lineEdit_2.setText(str(conta.limite)) 
+        def botaoExtrato(self):
+            #self.abrirTelaExtrato()
+            cpf = self.tela_login.lineEdit.text()
 
+            mensagem = 'history,'+cpf
+            cliente_socket.send(mensagem.encode())
+            control = cliente_socket.recv(1024).decode()
+            mensagem = ''
+            res = control.split(',')
+            if(control != 'false'):
+                self.tela_extrato.lineEdit_3.setText(res[0])
+                self.tela_extrato.lineEdit_4.setText(res[1])
+                self.tela_extrato.lineEdit_5.setText(res[2])
+                res1 = ''
+                for i in range(3,len(res)):
+                    res1.join(i)
+                self.tela_extrato.textEdit.setText(res1)
 
-    def botaoExtrato(self):
-        self.abrirTelaExtrato()
-
-        cpf = self.tela_login.lineEdit.text()
-        conta = self.cad.busca_cpf(cpf)
-        res = conta.historico.imprime([])
-        self.tela_extrato.lineEdit_3.setText(conta.titular.nome)
-        self.tela_extrato.lineEdit_4.setText(conta.titular.cpf)
-        self.tela_extrato.lineEdit_5.setText(str(conta.saldo))
-        self.tela_extrato.textEdit.setText(res)
-
-        
-    def botaoSacar(self):
-        cpf = self.tela_login.lineEdit.text()
-        valor = self.tela_saque.lineEdit_2.text()
-        conta = self.cad.busca_cpf(cpf)
-        
-        res = conta.saca(valor)
-        if res == False:
-            QMessageBox.information(None, 'GP Bank', 'Valor inválido!')
-        else:
-            QMessageBox.information(None, 'GP Bank', 'Saque efetuado!')
-
-        self.tela_home.lineEdit.setText(str(conta.saldo))
-        self.tela_home.lineEdit_2.setText(str(conta.limite)) 
-
-    def botaoTransfere(self):
-        cpf = self.tela_login.lineEdit.text()
-
-        numero_dest = self.tela_transferencia.lineEdit_3.text()
-        valor = self.tela_transferencia.lineEdit_2.text()
-        conta1 = self.cad.busca_cpf(cpf)
-        conta2 =self.cad.busca(numero_dest)
-
-        if(conta2 != None):
-            res = conta1.transferencia(conta2, valor)
+            
+        def botaoSacar(self):
+            cpf = self.tela_login.lineEdit.text()
+            valor = self.tela_saque.lineEdit_2.text()
+            conta = self.cad.busca_cpf(cpf)
+            
+            res = conta.saca(valor)
             if res == False:
                 QMessageBox.information(None, 'GP Bank', 'Valor inválido!')
             else:
-                QMessageBox.information(None, 'GP Bank', 'Tranferencia efetuada!')
-        else:
-            QMessageBox.information(None,'GP Bank', 'Conta destino não encontrada!')
+                QMessageBox.information(None, 'GP Bank', 'Saque efetuado!')
 
-        self.tela_home.lineEdit.setText(str(conta1.saldo))
-        self.tela_home.lineEdit_2.setText(str(conta1.limite)) 
+            self.tela_home.lineEdit.setText(str(conta.saldo))
+            self.tela_home.lineEdit_2.setText(str(conta.limite)) 
+
+        def botaoTransfere(self):
+            cpf = self.tela_login.lineEdit.text()
+
+            numero_dest = self.tela_transferencia.lineEdit_3.text()
+            valor = self.tela_transferencia.lineEdit_2.text()
+            conta1 = self.cad.busca_cpf(cpf)
+            conta2 =self.cad.busca(numero_dest)
+
+            if(conta2 != None):
+                res = conta1.transferencia(conta2, valor)
+                if res == False:
+                    QMessageBox.information(None, 'GP Bank', 'Valor inválido!')
+                else:
+                    QMessageBox.information(None, 'GP Bank', 'Tranferencia efetuada!')
+            else:
+                QMessageBox.information(None,'GP Bank', 'Conta destino não encontrada!')
+
+            self.tela_home.lineEdit.setText(str(conta1.saldo))
+            self.tela_home.lineEdit_2.setText(str(conta1.limite)) 
 
 
 
-    def botaoVoltar(self):
-        self.QtStack.setCurrentIndex(0)
-    
-    def abrirTelaLogin(self):
-        self.QtStack.setCurrentIndex(1)
+        def botaoVoltar(self):
+            self.QtStack.setCurrentIndex(0)
+        
+        def abrirTelaLogin(self):
+            self.QtStack.setCurrentIndex(1)
 
-    def abrirTelaCadastro(self):
-        self.QtStack.setCurrentIndex(2)
+        def abrirTelaCadastro(self):
+            self.QtStack.setCurrentIndex(2)
 
-    def abrirTelaDeposita(self):
-        self.QtStack.setCurrentIndex(3)
-    
-    def abrirTelaExtrato(self):
-        self.QtStack.setCurrentIndex(4)
-    
-    def abrirTelaSacar(self):
-        self.QtStack.setCurrentIndex(5)
+        def abrirTelaDeposita(self):
+            self.QtStack.setCurrentIndex(3)
+        
+        def abrirTelaExtrato(self):
+            self.QtStack.setCurrentIndex(4)
+        
+        def abrirTelaSacar(self):
+            self.QtStack.setCurrentIndex(5)
 
-    def abrirTelaTransferencia(self):
-        self.QtStack.setCurrentIndex(6)
+        def abrirTelaTransferencia(self):
+            self.QtStack.setCurrentIndex(6)
 
-    def abrirTelaHome(self):
-        self.QtStack.setCurrentIndex(7)
+        def abrirTelaHome(self):
+            self.QtStack.setCurrentIndex(7)
 
 
 
--- servidor.py
+++ servidor.py
@@ -1,4 +1,10 @@
+import sys
 import socket
+import os
+import mysql.connector as mysql
+
+conexao = mysql.connect(host = 'lacalhost', db='gpbank', user='root', passwd = '')
+cursor = conexao.cursor()
 
 host = 'localhost'
 port = 8000
@@ -12,22 +18,121 @@ print("aguardando conexão...")
 con, cliente = serv_socket.accept() #server aguardando conexão
 print("conectado")
 print("aguardando mensagem")
+enviar = ''
 
+while(enviar != 'sair'):
+    recebe = con.recv(1024).decode() #define os pacotes recebidos são de ate 1024 bytes 
+    variaveis = recebe.split(',')
+    if(variaveis[0] == 'cad'):
+        create = """CREATE TABLE IF NOT EXISTS contas(cpf INTEGER PRIMARY KEY, nome TEXT NOT NULL, sobrenome TEXT NOT NULL, numero INTEGER NOT NULL, limite FLOAT NOT NULL, senha VARCHAR(30) NOT NULL, saldo FLOAT NOT NULL);"""
+        cursor.execute(create)
+        nome = variaveis[1]
+        sobrenome = variaveis[2]
+        cpf = variaveis[3]
+        numero = variaveis[4]
+        limite = variaveis[5]
+        senha = variaveis[6]
+        saldo = 0.0
+        if(nome == '' or sobrenome == '' or cpf == '', numero == '' or limite == '' or senha == ''):
+            sql = "SELECT * FROM contas WHERE cpf = "+cpf+','
+            cursor.execute(sql)
+            c=None
+            for c in cursor:
+               c = c
+            if(c == None):
+                add = 'INSERT INTO contas(cpf, nome, sobrenome, numero, limite, senha, saldo) VALUES ('+cpf+',"'+nome+'", "'+sobrenome+'", '+numero+', '+limite+', "'+senha+'", '+saldo+');'
+                cursor.execute(add)
+                enviar = 'true'
+            else:
+                enviar = 'false'
 
-while(True):
-    recebe = con.recv(1024).decode() #define os pacotes recebidos são de ate 1024 bytes
-    print("mensagem recebida: " + recebe)
-    if(recebe != '/quit'):
-        enviar = input("Digite uma mensagem para enviar ao cliente: ")
-        con.send(enviar.encode())
-        if(enviar == '/quit'):
-            break
-    else:
-        serv_socket.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
-        print("aguardando conexão...")
-        con, cliente = serv_socket.accept() #server aguardando conexão
-        print("conectado")
-        print("aguardando mensagem")
+            con.send(enviar.encode())
 
-con.send('/quit'.encode())
+        elif(variaveis[0] == 'login'):
+            login = variaveis[1]
+            senha = variaveis[2]
+            if not(login == '' or senha == ''):
+                sql = 'SELECT * FROM contas WHERE cpf="'+cpf+'" AND senha="'+senha+'");'
+                acesso=None
+                cursor.execute(sql)
+                for acesso in cursor:
+                    acesso = acesso
+                if (acesso != None):
+                    enviar = str(acesso[0])
+                else:
+                    enviar="none"
+            else:
+                enviar = 'false'
+            
+            con.send(enviar.encode())
+
+        elif(variaveis[0] == 'deposito'):
+            valor = variaveis[1]
+            cpf = variaveis[2]
+            if not(valor == '' or cpf == ''):
+                
+                sql = "SELECT * FROM contas WHERE cpf="+cpf
+                cursor.execute(sql)
+
+                conta = None
+                for a in cursor:
+                    conta = a
+                if (conta != None):
+                    if(float(valor) > 0):
+                        new_saldo = float(valor) + conta[6]
+
+                        create1= """CREATE TABLE IF NOT EXISTS historico(cpf INTEGER PRIMARY KEY, 
+                        valor FLOAT NOT NULL, tipo_operacao TEXT NOT NULL, cpf_destino INTEGER);"""
+
+                        cursor.execute(create1)
+
+                        update_saldo = "UPDATE contas SET saldo="+new_saldo+' WHERE cpf='+cpf
+                        cursor.execute(update_saldo)
+
+                        registra = "INSERT INTO historico (cpf, valor, tipo_operacao, cpf_destino) VALUES ("+conta[1]+float(valor)+"'Deposito'"+conta[1]+")"
+
+                        cursor.execute(registra)
+
+                        enviar = 'true,'+new_saldo
+                    else:
+                        enviar = 'false,NaN'
+                else:
+                    enviar = 'false1,NaN'
+            else:
+                enviar = 'false2,NaN'
+            con.send(enviar.encode())
+
+        elif(variaveis[0] == 'saque'):
+            pass
+        elif(variaveis[0] == 'transfere'):
+            pass
+        elif(variaveis[0] == 'history'):
+            cpf = variaveis[1]
+
+            sql = "SELECT * FROM contas WHERE cpf="+cpf
+            cursor.execute(sql) 
+
+            conta = None
+            for i in cursor:
+                conta = i
+            if(conta != None):
+                sql = "SELECT * FROM historico WHERE cpf="+cpf
+                cursor.execute(sql) 
+                enviar = ''
+
+                enviar+conta[1]+','+conta[0]+','+conta[6]
+                for i in cursor:
+                    if(i[2] == 'Deposito' or i[2] == 'Saque'):
+                        enviar+','+i[2]+','+'de,'+i[1]
+                    else:
+                        enviar+','+i[2]+','+'de,'+i[1]+'para,'+i[3]
+            else:
+                enviar = 'false'
+           
+            con.send(enviar.encode())
+                    
+        else:
+            enviar = 'sair'
+            con.send(enviar.encode())
+
 serv_socket.close()
